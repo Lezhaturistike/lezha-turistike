8b00bb481d6c60b9dd6ee7ead1c6fb869ed2142e
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-21 01:29:08 +0200

    Activate separate English destinations page

--- next-app/app/en/destinacione/page.tsx
+++ next-app/app/en/destinacione/page.tsx
@@ -1,2 +1,7 @@
-import {redirect} from "next/navigation";
-export default function EnglishDestinacioneFallback(){redirect("/destinacione");}
+import {client} from "@/sanity/lib/client";
+import DestinacioneEnClient from "./DestinacioneEnClient";
+export const dynamic="force-dynamic";
+export default async function DestinationsPage(){
+ const destinacionet=await client.fetch(`*[_type=="destinacion"]|order(_createdAt asc){_id,titulli,titulliEn,kategoria,pershkrimi,pershkrimiEn,"fotoUrl":foto.asset->url,"galeriaUrls":galeria[].asset->url,burimi,harta}`);
+ return <DestinacioneEnClient destinacionet={destinacionet}/>;
+}
