04b4a76bec9728f446c0028f4f93fda99f2efc4d
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-21 01:35:24 +0200

    Fix navigation markup for Home and language switch

--- next-app/app/arkeologji/page.tsx
+++ next-app/app/arkeologji/page.tsx
@@ -26,7 +26,7 @@ export default async function ArkeologjiPage() {
         </Link>
 
         <nav className="nav" id="nav" aria-label="Navigimi kryesor">
-          <Link href="/">Home</Link>>
+          <Link href="/">Home</Link>
           <Link href="/destinacione">Destinacione</Link>
           <Link href="/galeri">Galeri</Link>
           <Link href="/histori">Histori</Link>
