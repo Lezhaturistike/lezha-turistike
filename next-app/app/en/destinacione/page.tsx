20b1524296f587f0d3fabc35e661f1c91f71979a
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 00:59:52 +0200

    Localize English destinations from Sanity

--- next-app/app/en/destinacione/page.tsx
+++ next-app/app/en/destinacione/page.tsx
@@ -1,7 +1,8 @@
 import {client} from "@/sanity/lib/client";
 import DestinacioneEnClient from "./DestinacioneEnClient";
+import {localizeContent} from "@/sanity/lib/localize";
 export const dynamic="force-dynamic";
 export default async function DestinationsPage(){
  const destinacionet=await client.fetch(`*[_type=="destinacion"]|order(_createdAt asc){_id,titulli,titulliEn,kategoria,pershkrimi,pershkrimiEn,"fotoUrl":foto.asset->url,"galeriaUrls":galeria[].asset->url,burimi,harta}`);
- return <DestinacioneEnClient destinacionet={destinacionet}/>;
+ return <DestinacioneEnClient destinacionet={localizeContent(destinacionet,"en")}/>;
 }
