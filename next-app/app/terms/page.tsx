e256be008c54fc6e5224faf102066ce5725ce1ab
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 00:56:59 +0200

    Connect legal page to Sanity CMS

--- next-app/app/terms/page.tsx
+++ next-app/app/terms/page.tsx
@@ -1,1 +1,5 @@
-import LegalPage from "@/app/components/LegalPage"; export default function Page(){return <LegalPage kind="terms"/>}
+import LegalPage, {type LegalContent} from "@/app/components/LegalPage";
+import {getPageContent} from "@/sanity/lib/content";
+import defaults from "@/sanity/content/terms.json";
+export const dynamic="force-dynamic";
+export default async function Page(){const content=await getPageContent("terms",defaults) as LegalContent;return <LegalPage kind="terms" content={content}/>}
