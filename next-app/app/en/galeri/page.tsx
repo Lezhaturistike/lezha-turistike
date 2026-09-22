5386c8ec62c8e93389d81464ae88a3fc5ee1ec88
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 10:20:08 +0200

    Add EN SEO metadata for galeri

--- next-app/app/en/galeri/page.tsx
+++ next-app/app/en/galeri/page.tsx
@@ -1,3 +1,5 @@
+import {pageMetadata} from "@/app/seo";
+export const metadata = pageMetadata("galeri", "en");
 import {localizeContent} from "@/sanity/lib/localize";
 import {client} from '@/sanity/lib/client'
 import GaleriEnClient from './GaleriEnClient'
