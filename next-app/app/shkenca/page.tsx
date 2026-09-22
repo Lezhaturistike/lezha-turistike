ee395205a2fc1637ca703675dbb7bf5c3f4e72e2
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 10:25:19 +0200

    Use H1 for primary page heading

--- next-app/app/shkenca/page.tsx
+++ next-app/app/shkenca/page.tsx
@@ -37,7 +37,7 @@ export default async function ShkencaPage() {
             <div>
               <span className="overline">{content.text1}</span>
 
-              <h2>{content.text2}</h2>
+              <h1>{content.text2}</h1>
             </div>
 
             <p>{content.text3}</p>
