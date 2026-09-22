d5d64bbcfc125e3281200d64ce35b7eb2cd89477
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 10:25:25 +0200

    Use H1 for primary page heading

--- next-app/app/partneret/page.tsx
+++ next-app/app/partneret/page.tsx
@@ -21,7 +21,7 @@ export default async function PartneretPage() {
             <div>
               <span className="overline">{content.text1}</span>
 
-              <h2>{content.text2}</h2>
+              <h1>{content.text2}</h1>
             </div>
 
             <p>{content.text3}</p>
