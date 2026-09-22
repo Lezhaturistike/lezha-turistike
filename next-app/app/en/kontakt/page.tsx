e973214f972470352ed95daebb2d1424640a3852
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-22 10:25:34 +0200

    Use H1 for primary page heading

--- next-app/app/en/kontakt/page.tsx
+++ next-app/app/en/kontakt/page.tsx
@@ -20,7 +20,7 @@ export default async function EnglishKontaktPage() {
           <div>
             <span className="overline">{content.text1}</span>
 
-            <h2>{content.text2}</h2>
+            <h1>{content.text2}</h1>
 
             <p>{content.text3}</p>
 
