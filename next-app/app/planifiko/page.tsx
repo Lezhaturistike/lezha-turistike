f350f403059ed6587a57b7f4b85931bda15bf6fd
Lezhaturistike <lezhalezha2024@gmail.com>
2026-09-24 20:23:29 +0200

    Add planner canonical and language metadata

--- next-app/app/planifiko/page.tsx
+++ next-app/app/planifiko/page.tsx
@@ -6,7 +6,7 @@ import PlannerClient,{type PlannerPlace} from "./PlannerClient";
 import {client} from "@/sanity/lib/client";
 import "./planifiko.css";
 
-export const metadata: Metadata={title:"Planifiko vizitën | Lezha Turistike",description:"Ndërto një itinerar të personalizuar në Lezhë sipas kohës, interesave, ushqimit dhe akomodimit."};
+export const metadata: Metadata={title:"Planifiko vizitën | Lezha Turistike",description:"Ndërto një itinerar të personalizuar në Lezhë sipas kohës, interesave, ushqimit dhe akomodimit.",alternates:{canonical:"https://lezhaturistike.com/planifiko",languages:{"sq-AL":"https://lezhaturistike.com/planifiko","en":"https://lezhaturistike.com/en/planifiko","x-default":"https://lezhaturistike.com/planifiko"}}};
 
 const plannerQuery=`*[_type in ["destinacion","kulinari","akomodim"] && defined(lokacioni.lat) && defined(lokacioni.lng)] | order(coalesce(plannerOrder,999) asc){
  "id":_id,"type":_type,"name":coalesce(titulli,emri),
